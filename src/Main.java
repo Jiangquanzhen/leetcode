26757eb685afcaf94d2a19e438c1ce26eb7a328b
unknown <1171436915@qq.com>
2019-05-15 23:33:30 +0800

    1111

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import com.N3;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        N3 n = new N3();
+        System.out.println(n.lengthOfLongestSubstring("abcabcbb"));
     }
 }
